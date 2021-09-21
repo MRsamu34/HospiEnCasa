46f7d189537180fc2da132da4f84cd0d1608a184
Samu Alvarez <samusoccer34@gmail.com>
2021-09-20 21:58:45 -0500

    Mejor indentacion

--- HospiEnCasa.App.Dominio/Entidades/Paciente.cs
+++ HospiEnCasa.App.Dominio/Entidades/Paciente.cs
@@ -7,20 +7,20 @@ namespace HospiEnCasa.App.Dominio
     public class Paciente : Persona
     {
         public string Direccion { get; set; }
-        /// Coordenada de la dirección del Paciente
+        // Coordenada de la dirección del Paciente
         public float Latitud { get; set; }
-        /// Coordenada de la dirección del Paciente
+        // Coordenada de la dirección del Paciente
         public float Longitud { get; set; }
         // Ciudad de residencia del Paciente
         public string Ciudad { get; set; }
         public DateTime FechaNacimiento { get; set; }
-        /// Relacion entre Paciente y su FamiliarDesignado para cuidarlo
+        // Relacion entre Paciente y su FamiliarDesignado para cuidarlo
         public FamiliarDesignado Familiar { get; set; }
-        /// Relacion entre Paciente y la  Enfermera asignada
+        // Relacion entre Paciente y la  Enfermera asignada
         public Enfermera Enfermera { get; set; }
-        /// Relacion entre Paciente y el Medico que lo atiende
+        // Relacion entre Paciente y el Medico que lo atiende
         public Medico Medico { get; set; }
-        /// Relacion entre Paciente y su Historia clínica
+        // Relacion entre Paciente y su Historia clínica
         public Historia Historia { get; set; }
         /// <summary>
         ///  Referencia a la lista de signos vitales de un Paciente 
