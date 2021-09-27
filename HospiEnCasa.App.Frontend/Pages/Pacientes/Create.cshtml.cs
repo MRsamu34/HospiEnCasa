858a89528bcdee77e52bd235358223938deb181a
Samu Alvarez <samusoccer34@gmail.com>
2021-09-27 08:24:09 -0500

    Asignar Medico

---
+++ HospiEnCasa.App.Frontend/Pages/Pacientes/Create.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
+namespace HospiEnCasa.App.Frontend.Pages.Pacientes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IRepositorioPaciente _repoPaciente;
+        public Paciente pacientes { get; set; }
+        public CreateModel(IRepositorioPaciente repoPaciente)
+        {
+            _repoPaciente = repoPaciente;
+        }
+
+        public void OnGet()
+        {
+            paciente = new Paciente();
+        }
+    }
+}
